3bb0e79c8ce6e43b214a8894ed946eef4065323d
kosuke-sonsan <sonbb313@gmail.com>
2022-06-26 00:47:51 +0900

    last-commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,23 +5,23 @@
   <table>
     <div class="text_table">
       <tr>
-        <th>作成日</th>
-        <th>タスク名</th>
-        <th>更新</th>
-        <th>削除</th>
+        <th class="table_th1">作成日</th>
+        <th class="table_th2">タスク名</th>
+        <th class="table_th3">更新</th>
+        <th class="table_th4">削除</th>
       </tr>
     </div>
     @foreach($items as $item)
       <tr>
-        <td>{{$item->created_at}}</td>
+        <td class="table_td1">{{$item->created_at}}</td>
         <form action="{{ route('todo.update', ['id'=>$item->id]) }}" method="post">
           @csrf
-          <td><input type="text" name="content" value="{{$item->content}}" class="text2"></td>
-          <td><button class="update">更新</button></td>
+          <td class="table_td2"><input type="text" name="content" value="{{$item->content}}" class="text2"></td>
+          <td class="table_td3"><button class="update">更新</button></td>
         </form>
         <form action="{{ route('todo.delete', ['id'=>$item->id]) }}" method="post">
           @csrf
-          <td><button class="delete">削除</button></td>
+          <td class="table_td4"><button class="delete">削除</button></td>
         </form>
       </tr>
     @endforeach
